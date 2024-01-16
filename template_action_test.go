4efd5d943ae8411cb44f5664f9d1f99ac2260402
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-01-16 20:24:05 +0700

    add template action comparator operation

--- template_action_test.go
+++ template_action_test.go
@@ -20,11 +20,34 @@ func TemplateActionIf(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func TemplateActionOperator(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("./template/comparator.gohtml"))
+
+	err := t.ExecuteTemplate(w, "comparator.gohtml", map[string]interface{}{
+		"Title":      "Template action comparator",
+		"FinalValue": 10,
+	})
+	if err != nil {
+		panic(err)
+	}
+}
+
 func TestTemplateActionIf(t *testing.T) {
 	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
 	recorder := httptest.NewRecorder()
 
 	TemplateActionIf(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+
+	fmt.Println(string(body))
+}
+
+func TestTemplateActionOperator(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateActionOperator(recorder, request)
 
 	body, _ := io.ReadAll(recorder.Result().Body)
 
