2393010b3ebb7219472e3378f278ade49c2f2eda
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-01-16 14:02:50 +0700

    add simple template html

---
+++ template_test.go
@@ -0,0 +1,35 @@
+package go_web
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func SimpleHtml(w http.ResponseWriter, r *http.Request) {
+	templateText := `<html><body>{{.}}</body></html>`
+
+	t, err := template.New("SIMPLE").Parse(templateText)
+	if err != nil {
+		panic(err)
+	}
+
+	err = t.ExecuteTemplate(w, "SIMPLE", "Hello World")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestSimpleHtml(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	SimpleHtml(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+
+	fmt.Println(string(body))
+}
