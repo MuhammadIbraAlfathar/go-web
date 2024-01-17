7b121fa06513fd1922e6169d5e76b5cb5d121945
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-01-17 13:40:01 +0700

    add redirect out

--- redirect_test.go
+++ redirect_test.go
@@ -11,13 +11,18 @@ func RedirectTo(w http.ResponseWriter, r *http.Request) {
 }
 
 func RedirectFrom(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/redirect_to", http.StatusTemporaryRedirect)
+	http.Redirect(w, r, "/redirect-to", http.StatusTemporaryRedirect)
+}
+
+func RedirectOut(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://www.udemy.com/course/pemrograman-go-lang-pemula-sampai-mahir/learn/lecture/26415468#questions", http.StatusTemporaryRedirect)
 }
 
 func TestRedirect(t *testing.T) {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/redirect-from", RedirectFrom)
 	mux.HandleFunc("/redirect-to", RedirectTo)
+	mux.HandleFunc("/redirect-out", RedirectOut)
 
 	server := http.Server{
 		Addr:    "localhost:8080",
