5926a6e75f3e9b0bceabd0e3df64a07ccc30c408
MuhammadIbraAlfathar <ibraalfathar31@gmail.com>
2024-01-17 13:37:11 +0700

    add redirect

---
+++ redirect_test.go
@@ -0,0 +1,31 @@
+package go_web
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+func RedirectTo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
+}
+
+func RedirectFrom(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/redirect_to", http.StatusTemporaryRedirect)
+}
+
+func TestRedirect(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/redirect-from", RedirectFrom)
+	mux.HandleFunc("/redirect-to", RedirectTo)
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
